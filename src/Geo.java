f4e120f6609bcfba0c38d3462e6b8b9f257f5d00
Mateusz Konieczny <matkoniecz@gmail.com>
2014-02-15 13:38:39 +0100

    Add GetDistanceBetweenCoordinates
    Calculation is using haversine formula.
    
    Add relevant tests.

---
+++ src/Geo.java
@@ -0,0 +1,29 @@
+public class Geo {
+    private final static double earthRadius = 6371;
+    /**
+     * Computes distance between two points on Earth using Haversine formula
+     *
+     * Earth is assumed to be sphere, errors from assuming spherical geometry might be up to 0.55% crossing the equator
+     * source: https://en.wikipedia.org/wiki/Haversine_formula and http://www.movable-type.co.uk/scripts/latlong.html
+     *
+     * @param lat1 latitude of the first point
+     * @param lon1 longitude of the first point
+     * @param lat2 latitude of the second point
+     * @param lon2 longitude of the second point
+     * @return Distance between points, in kilometers
+     */
+    public static double GetDistanceBetweenCoordinates(double lat1, double lon1, double lat2, double lon2){
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+        double dLat = lat2-lat1;
+        double dLon = lon2-lon1;
+
+        double a; //square of half the chord length between the points
+        a =  Math.sin(dLat/2) * Math.sin(dLat/2);
+        a += Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat2) * Math.cos(lat1);
+        double angularDistanceInRadians = Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return 2 * earthRadius * angularDistanceInRadians;
+    }
+}
---
+++ tests/GeoTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+public class GeoTest {
+    @org.junit.Test
+    public void testGetDistanceBetweenCoordinates() throws Exception {
+        double almostZero = 1/1000; //1 m error is perfectly acceptable
+
+        String distanceToItself = "point has 0 distance to itself";
+        for(int i = 0; i<1000; i++){
+            double lat = Math.random() * 180 - 90;
+            double lon = Math.random() * 180 - 90;
+            assertEquals(distanceToItself, 0, Geo.GetDistanceBetweenCoordinates(lat, lon, lat, lon), almostZero);
+        }
+
+        double distance;
+        //expected values were acquired from http://www.movable-type.co.uk/scripts/latlong.html
+        distance = Geo.GetDistanceBetweenCoordinates(50.03, 5.42, 58.38, 3.04);
+        assertEquals("distance check", distance, 941.1, 0.05);
+        distance = Geo.GetDistanceBetweenCoordinates(0, 0, 90, 0);
+        assertEquals("distance check", distance, 10010, 5);
+        distance = Geo.GetDistanceBetweenCoordinates(0, 0, 0, 180);
+        assertEquals("distance check", distance, 20020, 5);
+        distance = Geo.GetDistanceBetweenCoordinates(-50.03, 5.42, 58.38, 3.04);
+        assertEquals("distance check", distance, 12060, 5);
+        distance = Geo.GetDistanceBetweenCoordinates(50.0764, 19.9658, 19.9658, 3);
+        assertEquals("distance check", distance, 3668, 0.5);
+        distance = Geo.GetDistanceBetweenCoordinates(50.0764, 19.9658, 51.0764, 20.9658);
+        assertEquals("distance check", distance, 131.7, 0.05);
+    }
+}
