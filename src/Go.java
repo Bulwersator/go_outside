f579f8916e4b844f3b59593bf7705459e72b5275
Mateusz Konieczny <matkoniecz@gmail.com>
2014-02-17 21:25:58 +0100

    use proper thread handling

--- src/Go.java
+++ src/Go.java
@@ -6,7 +6,14 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Go {
-    public static void main(String [ ] args) throws ParseException {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Go();
+            }
+        });
+    }
+    public Go() {
         Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
         int availableScreenWidth = winSize.width;
         int availableScreenHeight = winSize.height;
@@ -14,7 +21,7 @@ public class Go {
         //f.setUndecorated(true);
         f.setResizable(false);
         f.setAlwaysOnTop(true);
-        JTextField textField = new JTextField("loading...");
+        final JTextField textField = new JTextField("loading...");
         f.add(textField);
         f.setMinimumSize(new Dimension(300, 10));
         f.pack();
@@ -22,10 +29,30 @@ public class Go {
         f.setLocation(availableScreenWidth - f.getWidth(), availableScreenHeight - f.getHeight());
         f.setVisible(true);
 
-        final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        final Calendar c = Calendar.getInstance();
-        c.setTime(df.parse("2005-05-26"));
-        Geohash test = new Geohash(50, 19, c);
-        textField.setText(test.getResult());
+        class GeohashLoader extends SwingWorker<Geohash, Void> {
+            @Override
+            public Geohash doInBackground() {
+                final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+                final Calendar c = Calendar.getInstance();
+                try {
+                    c.setTime(df.parse("2005-05-26"));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                return new Geohash(50, 19, c);
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    final Geohash test = get();
+                    textField.setText(test.getResult());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        (new GeohashLoader()).execute();
     }
 }
