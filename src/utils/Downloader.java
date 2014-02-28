f28a4b2409fe1fd831fade8ada9b795cb85b14a3
Mateusz Konieczny <matkoniecz@gmail.com>
2014-02-28 13:30:13 +0100

    smarter caching
    it is now recognising that multiple threads requested the same URL, it will be fetched only once - not multiple times as before

--- src/utils/Downloader.java
+++ src/utils/Downloader.java
@@ -1,14 +1,17 @@
 package utils;
 
+import org.joda.time.DateTime;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Downloader {
     static Map<URL, String> cache = null;
+    static Map<URL, DateTime> downloadInProgress = null;
     /**
      * Downloads specified resource
      * @param url Uniform resource locator
@@ -18,11 +21,31 @@ public class Downloader {
     public static String fetchURL(String url) throws IOException {
         URL address = new URL(url);
         if(cache == null){
-            cache = new HashMap<>();
+            cache = new ConcurrentHashMap<>();
+        }
+        if(downloadInProgress == null){
+            downloadInProgress = new ConcurrentHashMap<>();
         }
         if(cache.containsKey(address)){
             return cache.get(address);
         }
+        if(downloadInProgress.containsKey(address)){
+            while (true){
+                if(downloadInProgress.get(address).isBefore(DateTime.now().minusMinutes(1))){
+                    downloadInProgress.remove(address);
+                    break;
+                }
+                if(cache.containsKey(address)){
+                    return cache.get(address);
+                }
+                try {
+                    Thread.sleep(100);
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+        downloadInProgress.put(address, DateTime.now());
         System.out.println(address);
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(address.openStream())
