f4d40588703c410dd6cf8134a904bc3ba0f1a8d1
Mateusz Konieczny <matkoniecz@gmail.com>
2014-03-01 21:22:15 +0100

    fix ifs without braces

--- src/net/exclaimindustries/tools/CharToByte.java
+++ src/net/exclaimindustries/tools/CharToByte.java
@@ -64,13 +64,16 @@ public class CharToByte {
         String s;
         int i;
         for (i = 0; i < bytes.length; i++) {
-            if (i % 32 == 0 && i != 0)
+            if (i % 32 == 0 && i != 0) {
                 sb.append("\n");
+            }
             s = Integer.toHexString(bytes[i]);
-            if (s.length() < 2)
+            if (s.length() < 2) {
                 s = "0" + s;
-            if (s.length() > 2)
+            }
+            if (s.length() > 2) {
                 s = s.substring(s.length() - 2);
+            }
             sb.append(s);
         }
         return sb.toString();
